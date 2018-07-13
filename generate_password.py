5c6d41d79397658c812e06d29ad376d3575686aa
Alec <asaunders@admarketplace.com>
2018-07-13 16:24:40 -0400

    Comments cleanup

--- generate_password.py
+++ generate_password.py
@@ -4,26 +4,22 @@ import vertica_sdk
 import random
 
 class generate_password(vertica_sdk.ScalarFunction):
-    """Return the sum of two integer columns"""
+    """Return random password of N length"""
 
     lower_case = 'abcdefghijklmnopqrstuvwxyz'
     upper_case = lower_case.upper()
     digits = '1234567890'
     symbols = "#!+*?,./=~-$_: \"'%&();<>@`[]\^|{}"
 
-    def __init__(self):
-        pass
-
     def setup(self, server_interface, col_types):
         pass
 
     def processBlock(self, server_interface, arg_reader, res_writer):
-        # Writes a string to the UDx log file.
         while(True):
             if arg_reader.isNull(0):
                 raise ValueError("I found a NULL!")
             else:
-                pass_len = arg_reader.getInt(0)  # Input column
+                pass_len = arg_reader.getInt(0)  # Password length input column
 
             pass_len = 8 if pass_len < 8 else pass_len
             pass_len = 100 if pass_len > 100 else pass_len
